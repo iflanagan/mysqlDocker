c405a1d31cd12141cfde4e291a0d2e69889fe2d3
Ian Flanagan <ianianf@yahoo.com>
2023-10-23 09:07:34 -0500

    fixed init.sql

--- init.sql
+++ init.sql
@@ -17,17 +17,13 @@ INSERT INTO users (username,password, email) VALUES
     ('ronit','test@#$123', 'ronit@testim.io'),
     ('ianflanagan','test@#$123', 'ian@testim.io');
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'testim?5432'; 
+ALTER USER 'root'@'localhost' IDENTIFIED WITH 'caching_sha2_password' BY 'testim?5432';
 
 CREATE USER 'newuser'@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'testim123';
 
---GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'%' IDENTIFIED BY 'testim123';
---GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'testim?5432';
-
-GRANT SELECT ON DEMO.* TO 'newuser'@'%';
-
 GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+
 
 FLUSH PRIVILEGES;
 
